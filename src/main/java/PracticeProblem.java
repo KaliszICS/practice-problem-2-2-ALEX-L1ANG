c836420b65f12c2188b5b5e2edb80c8b1685ab3a
ALEX-L1ANG <343293684@tdsb.ca>
2026-03-11 15:32:48 +0000

    update1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,35 @@
+/*
+Author: Alex Liang
+Lesson: 2.2 - More Functions
+Date Created: March 11, 2026
+Date Last Modified: March 11, 2026
+*/
+
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	public static boolean isEven(int num){
+		return num%2 == 0;
+	}
+	
+	public static boolean isOdd(int num){
+		return num%2 != 0;
+	}
+	
+	public static boolean isPositive(int num){
+		return num > 0;
+	}
+
+	public static boolean isNegative(int num){
+		return num < 0;
+	}
+
+	public static int combinedLength(String word1, String word2){
+		int cLen = word1.length() + word2.length();
+		return cLen;
+	}
 }
